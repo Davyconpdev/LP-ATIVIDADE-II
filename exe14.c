4361f98c8066f9b3d194e9b7a0eac1be792adc61
Davyconpdev <143567744+Davyconpdev@users.noreply.github.com>
2023-11-08 16:47:16 -0300

    Add files via upload

---
+++ exe14.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <ctype.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    int numero = 0, par = 0, impar = 0, i = 0;
+    float somaPar = 0, somaImpar = 0, mediaPar = 0, mediaImpar = 0;
+
+    do
+    {
+        printf("digite o %dº número:", i + 1);
+        scanf("%d", &numero);
+        i++;
+
+        if (numero % 2 == 0)
+        {
+            par++;
+            somaPar += i;
+        }
+        else if (numero % 2 == 1)
+        {
+            impar++;
+            somaImpar += i;
+        }
+        fflush(stdin);
+
+    } while (numero >= 0);
+
+    mediaPar = somaPar / par;
+    mediaImpar = somaImpar / impar;
+    printf("\n");
+    printf("números pares: %d\n", par);
+    printf("números ímpares: %d\n", impar);
+    printf("média números pares: %.1f\n", mediaPar);
+    printf("média números ímpares: %.1f\n", mediaImpar);
+
+    return 0;
+}
